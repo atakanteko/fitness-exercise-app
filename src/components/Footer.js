62e224a4d6029edfa7eb91eecd5ab5f620e1fed7
atakantekoglu <atakantekoglu@gmail.com>
2022-06-14 23:11:08 +0300

    [feat] footer completed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import {Box, Stack, Typography} from "@mui/material";
 
 const Footer = () => {
+    const getDate = () => {
+        const date = new Date();
+        return date.getFullYear();
+    }
     return (
-        <div>
-            Footer
-        </div>
+        <Box mt="80px" bgcolor="#fff3f4">
+            <Stack gap="40px" alignItems="center" px="40px" py="24px">
+                <Typography variant="h5" color="textPrimary">
+                    © {getDate()} - Atakan TEKO
+                </Typography>
+            </Stack>
+        </Box>
     );
 };
 
