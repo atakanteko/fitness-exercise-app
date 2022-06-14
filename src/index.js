066359328f788c6ef92a82c3ae3b2b0963830802
atakantekoglu <atakantekoglu@gmail.com>
2022-06-14 11:29:31 +0300

    [feat] file and folder structure

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter} from "react-router-dom";
 
 import App from "./App";
 
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+const rootElement = ReactDOM.createRoot(document.getElementById("root"));
+
+rootElement.render(<BrowserRouter>
+    <App />
+</BrowserRouter>);
